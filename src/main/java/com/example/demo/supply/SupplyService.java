0ca5f6acb21b0e8d0af8fe9c159763be8d3bee98
hasaway2 <hasaway2@naver.com>
2025-05-19 11:05:07 +0900

    비품 감소 dao 호출 생략된 것 추가

--- src/main/java/com/example/demo/supply/SupplyService.java
+++ src/main/java/com/example/demo/supply/SupplyService.java
@@ -36,6 +36,7 @@ public class SupplyService {
     Supply supply = supplyDao.findBySno(sno).orElseThrow(()->new EntityNotFoundException("비품를 찾을 수 없습니다"));
     if(supply.getQuantity()<=1)
       throw new JobFailException("더이상 감소할 수 없습니다");
+    supplyDao.dec(sno);
     return supplyDao.findBySno(sno).get().getQuantity();
   }
 
